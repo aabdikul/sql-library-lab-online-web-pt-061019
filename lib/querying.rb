a5c97ddd4896319c9156722e189c5f7172a8fe25
Aida Abdikulova <abdikulova@gmail.com>
2019-09-06 02:16:40 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,7 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
   INNER JOIN characters ON series.author_id = characters.author_id
-  GROUP BY species WHERE species = 'human'
+  GROUP BY species
   ORDER BY COUNT(species) DESC LIMIT 1"
 end
 
